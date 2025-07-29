1ee90ce8c7bac6bd6cd0d94c985d38326678bc43
John Rimuel Bigata <rimuel.dev@gmail.com>
2025-07-29 09:59:02 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "claritybudgets-tga72",
+  "appId": "1:234902919003:web:34a2e719d00b431cd02c7e",
+  "storageBucket": "claritybudgets-tga72.firebasestorage.app",
+  "apiKey": "AIzaSyDMBRHcEOokh4fnOaZDcTt1x4NlonYFHGM",
+  "authDomain": "claritybudgets-tga72.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "234902919003"
 };
 
 // Initialize Firebase
